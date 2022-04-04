6aa29d435d43d20d0f07195e1df53ab3d4138489
= <=>
2022-04-04 20:22:36 +0530

    Milestone 2

--- DBMS Project/routes/index.js
+++ DBMS Project/routes/index.js
@@ -2,6 +2,7 @@ const { routes } = require("../app");
 const router = require("express").Router();
 
 router.use("/api/users", require('./users'));
-router.use("/api/movies", require('./movie'));
+router.use("/api/movie", require('./movie'));
+router.use("/api/screen", require('./screen'));
 
 module.exports = router;
--- DBMS Project/routes/screen/index.js
+++ DBMS Project/routes/screen/index.js
@@ -2,23 +2,16 @@ const router = require('express').Router()
 const connection = require('../../database');
 
 router.post('/add', (req, res) => {
-    const sql = `INSERT INTO movie SET ?`
+    const sql = `INSERT INTO screen SET ?`
     connection.query(sql, req.body , (err , rows) => {
         if(err) throw err;
         res.json(rows);
     })
 });
 
-router.get("/display/:id", (req, res) => {
-    const sql = 'SELECT * FROM display WHERE mid = ?'
-    connection.query(sql, req.params.id , (err , rows) => {
-        if(err) throw err;
-        res.json(rows);
-    })
-})
 
 router.get('/:id', (req, res) => {
-    const sql = `SELECT * FROM movie WHERE mid=${req.params.id}`
+    const sql = `SELECT * FROM screen WHERE mid=${req.params.id}`
     connection.query(sql , (err , rows) => {
         if(err) throw err;
         res.json(rows);
@@ -26,7 +19,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    const sql = `DELETE FROM movie WHERE mid = ?`
+    const sql = `DELETE FROM screen WHERE mid = ?`
     connection.query(sql, [req.params.id], (err, rows) => {
         if(err) throw err;
         res.json(rows);
@@ -34,7 +27,7 @@ router.delete('/:id', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-    const sql = `SELECT * FROM movie`
+    const sql = `SELECT * FROM screen`
     connection.query(sql , (err , rows) => {
         if(err) throw err;
         res.json(rows);
